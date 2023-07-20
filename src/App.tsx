ca0d84b30e287aadee61529b1db74302ddd89122
ogdakke <daniel.wargh1@gmail.com>
2023-07-20 19:28:53 +0300

    dev env

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,15 @@ import { Loading } from "./Components/ui/loading";
 
 const FormComponent = React.lazy(async () => await import("./Components/form"));
 
+const isDev = import.meta.env.DEV;
+
 function App() {
     return (
         <main className="main">
             <div className="wrapper">
                 <div className="flex-center" style={{ gap: "1rem" }}>
                     <LogoIcon width={40} height={40} />
-                    <h1>Salasanakone</h1>
+                    <h1>Salasanakone{isDev ? " Dev Env" : null}</h1>
                 </div>
                 <Suspense fallback={<Loading />}>
                     <ErrorBoundary
