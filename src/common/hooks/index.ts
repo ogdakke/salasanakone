d49cc0e1396a47a01200a1aa58ba31bfd4eb95b2
ogdakke <daniel.wargh1@gmail.com>
2023-11-04 20:43:16 +0200

    remove: redux files

--- src/common/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-// Override the default dispatch and useSelector hooks with the typed ones
-export {
-  useAppDispatch as useDispatch,
-  useAppSelector as useSelector,
-} from "@/common/hooks/redux-hooks"
