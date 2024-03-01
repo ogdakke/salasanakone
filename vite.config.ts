a00af3df630455bc5fa0aec5f61f8bab724629be
ogdakke <daniel.wargh1@gmail.com>
2024-03-01 01:18:58 +0000

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -74,7 +74,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src/"),
+      "@": path.resolve(__dirname, "./src"),
       "@services": path.resolve(__dirname, "./src/services"),
       "@config": path.resolve(__dirname, "./config"),
     },
