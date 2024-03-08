8f78ec8cfb04ed0e9fcc820bc8c3ff77c5b6d3c7
ogdakke <daniel.wargh1@gmail.com>
2024-03-08 19:47:49 +0200

    Revert "lazy load framer"
    This reverts commit 1e54ca47b7b4aa8df7fab21a859e3e4cdecc40a6.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { FormProvider } from "@/Components/FormContext"
-import { LazyMotion, MotionConfig } from "framer-motion"
+import { LazyMotion, MotionConfig, domAnimation } from "framer-motion"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
-const domAnimation = (await import("@/FramerFeature")).default
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <LazyMotion features={domAnimation} strict>
